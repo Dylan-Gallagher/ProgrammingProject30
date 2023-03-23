7aeb39ca8fad12a24c0886dcf2a6c3202ea29d20
Dylan Gallagher <76841713+Dylan-Gallagher@users.noreply.github.com>
2023-03-23 15:01:50 +0000

    Refactored pre-processed_data.py

--- pre-processed-data.py
+++ pre-processed-data.py
@@ -11,10 +11,10 @@ Original file is located at
 # Code breaks for 100k and flights_full due to one row having time of "5" which
 #   is meant to represent 00:05
 
-
 import pandas as pd
 
 files = ["flights2k.csv", "flights10k.csv"]
+
 
 def add_delay_column(df):
   print(df.head())
@@ -22,6 +22,7 @@ def add_delay_column(df):
   df['time_delayed'] = pd.to_datetime(df['DEP_TIME'], format='%H%M', errors='coerce') - pd.to_datetime(df['CRS_DEP_TIME'], format='%H%M')
   df['time_delayed'] = df['time_delayed'].dt.total_seconds().div(60).astype(float)
   return df
+
 
 for file in files:
   print(file)
